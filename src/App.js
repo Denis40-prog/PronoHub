82fa9394623c5be570aa465aae7a5841ac358bb3
NoahRazzaq <84766310+NoahRazzaq@users.noreply.github.com>
2023-10-30 15:06:12 +0100

    :lipstick: adding tailwind + user card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import PersonnalCardGrid from './components/atomes/PersonnalCard/PersonnalCard';
 import PersonnalCard from './components/atomes/PersonnalCard/PersonnalCard';
 import Header from './components/molecules/header/Header';
 
@@ -9,9 +10,13 @@ function App() {
       <Header />
       <header className="App-header">
         <h1>{title}</h1>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
-        <PersonnalCard />
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
       </header>
+        <div>
+        <PersonnalCardGrid></PersonnalCardGrid>
+        </div>
+
+
     </div>
   );
 }
