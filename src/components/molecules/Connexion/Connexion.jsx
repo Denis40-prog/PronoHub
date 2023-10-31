0aaeeee40c7f22bd20b81be5047c5db7590c31d5
alexjnj1301 <114980961+alexjnj1301@users.noreply.github.com>
2023-10-31 16:51:33 +0100

    ended connexion

---
+++ src/components/molecules/Connexion/Connexion.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import logo from "../../../assets/images/LogoPronohub.png";
+
+
+const Connexion = () => {
+    return(
+        <div className="flex justify-center">
+          <div className="shadow-caca shadow-secondary rounded-xl w-2/6 mt-12 bg-primary">
+            <div className=" h-3/6 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img className="mx-auto h-20 w-auto" src={logo} alt="" />
+                    <h2 className="mt-10 text-center font-bold leading-9 tracking-tight">Conne<span className="text-accent">xion</span></h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST">
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-accent">Email</label>
+                            <div className="mt-2">
+                            <input id="email" name="email" type="email" placeholder="Entrez votre email" required className="block w-full placeholder:italic p-2 rounded-xl border-2 border-accent py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-accent">Password</label>
+                            </div>
+                            <div className="mt-2">
+                            <input id="password" name="password" type="password" placeholder="Entrez votre mot de passe" required className="block w-full placeholder:italic p-2 rounded-xl border-2 border-accent py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6" />
+                            </div>
+                        </div>
+
+                        <div className="flex w-full place-content-center">
+                            <button type="submit" className="flex w-fit font-semibold justify-center text-black rounded-3xl text-sm px-5 py-2.5 mr-2 mb-2 bg-accent">Se connecter</button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        <a className="hover:text-accent cursor-pointer">Créer un compte</a> / 
+                        <a className="hover:text-accent cursor-pointer"> S'inscrire</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default Connexion;
