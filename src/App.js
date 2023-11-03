ac4fb03aa415ca99abf3a9b0d9748f899005e149
NouveauPseudo <149676226+NouveauPseudo@users.noreply.github.com>
2023-11-03 10:38:05 +0100

    first page with cookie restriction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import './App.css';
 import React, { useState } from 'react';
 import Header from './components/molecules/header/Header';
+import {CookiesProvider, useCookies} from "react-cookie"
+import FirstScreen from './components/organisms/FirstScreen';
 
 
 function App() {
   const [activePage, setActivePage] = useState('Connexion')
+  const [cookies, setCookie] = useCookies(['restriction']);
+
+  if(cookies.restriction === undefined){
+    setCookie('restriction', true,)
+  }
 
   return (
+    <CookiesProvider>
+
     <div className="App">
-      <Header setAppActivePage={setActivePage}/>
-      <body className='bg-black h-screen'>
-        {activePage}
-      </body>
+  <body className='bg-black h-screen'>
+
+      {cookies.restriction ? 
+        
+      <FirstScreen>
+       </FirstScreen>
+       :
+       <>
+  <Header setAppActivePage={setActivePage}/>
+    {activePage}
+  </> 
+}
+</body>
+
     </div>
+
+    </CookiesProvider>
   );
 }
 
---
+++ src/components/organisms/FirstScreen.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Logo from '../../assets/images/LogoPronoHubNoir.png'
+import ButtonFirstScreen from '../molecules/ButtonFirstSCreen/ButtonFirstScreen'
+import { useCookies } from 'react-cookie';
+
+function FirstScreen() {
+
+    const [cookies, setCookie] = useCookies(['restriction'])
+
+    function HandleRestriction(bool){
+      if (bool === true){
+        window.history.back()
+        }else if ( bool === false){
+            setCookie("restriction", false)
+        }
+    }
+
+  return (
+    <div className='flex flex-col items-center justify-center place-content-center min-h-full'>
+        <img src={Logo} alt="logo" className=' w-56' />
+        <h1 className='font-bold'>Avez-vous 18 ans ?</h1>
+        <h2>PronoHub est un site qui propose des paris sportif. <br/>
+        Vous devez avoir 18 ans ou plus pour entrer.
+        </h2>
+        <div className='flex flex-row p-10'>
+        <ButtonFirstScreen style="bg-accent" HandleRestriction={HandleRestriction} Bool={false}>
+            J'ai 18 ans ou plus - Entrer
+        </ButtonFirstScreen>
+        <ButtonFirstScreen style="text-white bg-primary" HandleRestriction={HandleRestriction} Bool={true}>
+            J'ai moins de 18 ans - Sortir
+        </ButtonFirstScreen>
+        </div>
+       
+
+
+    </div>
+  )
+}
+
+export default FirstScreen
