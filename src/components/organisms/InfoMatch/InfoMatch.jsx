ac3eaca21f3486c7f86413584fe2dd12c6d1e7ce
alexjnj1301 <114980961+alexjnj1301@users.noreply.github.com>
2023-11-21 20:59:17 +0100

    info match integration

--- src/components/organisms/InfoMatch/InfoMatch.jsx
+++ src/components/organisms/InfoMatch/InfoMatch.jsx
@@ -8,9 +8,10 @@ import Slogan from "../../atomes/Slogan/Slogan";
 import { getRequest } from "../../../services/ApiCallService";
 
 const InfoMatch = ({...props}) => {
-    const [matchInfo, setMatchInfo] = useState([]);
     const [isPopupOpen, setIsPopupOpen] = useState(false);
     const [teamToBet, setTeamToBet] = useState("");
+    const [matchInfo, setMatchInfo] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function fetchData() {
@@ -22,18 +23,22 @@ const InfoMatch = ({...props}) => {
                         console.log('data ', data);
                         props.openSnackBar('Les informations sur le match ont bien été récupérées')
                         setMatchInfo(data);
+                        setIsLoading(false);
                     }, error => {
                         console.error('Error parsing JSON:', error);
+                        setIsLoading(false);
                     });
                 } else {
                     console.error(`Request failed: ${response.status}`, response);
+                    setIsLoading(false);
                 }
             } catch (error) {
                 console.error('Error:', error);
+                setIsLoading(false);
             }
         }
         fetchData();
-    }, [])
+    }, [props.matchId])
 
     const openPopup = (team) => {
         setIsPopupOpen(true);
@@ -46,48 +51,50 @@ const InfoMatch = ({...props}) => {
 
     return (
         <div>
-            <Slogan ></Slogan >
-            <h2 className="flex col-auto ml-3 mt-10 items-center">
-                <FaAngleLeft className="text-accent cursor-pointer"
-                    onClick={() => props.setPage("Accueil")}
-                    data-tooltip-id="getBack"
-                    data-tooltip-content={'Retour'}/>
-                Match {matchInfo.teamId1} <span className="mr-2 ml-2 text-accent"> vs </span> {matchInfo.teamId2}
-            </h2>
-            <Tooltip id="getBack" />
-            <div className="flex flex-col mt-10">
-                <div className="grid grid-flow-col justify-stretch mt-10 h-fit w-full place-items-center">
-                    <div className="flex flex-col space-y-8">
-                        <img src={logoTeam1} alt="" className="h-72 drop-shadow-logo" />
-                        <button
-                            className="bg-accent text-white font-bold py-2 px-4 rounded"
-                            onClick={() => openPopup(matchInfo.teamId1)}
-                            data-tooltip-id="betTeam1"
-                            data-tooltip-content={`Parier sur ${matchInfo.teamId1}`}
-                        >
-                            <Tooltip id="betTeam1" />
-                            {matchInfo.teamId1}
-                        </button>
+            {isLoading ? (
+                <p>Chargement en cours...</p>
+            ) : ( 
+                <div>
+                    <Slogan />
+                    <h2 className="flex col-auto ml-3 mt-10 items-center">
+                        <FaAngleLeft className="text-accent cursor-pointer"
+                            onClick={() => props.setPage("Accueil")}
+                            data-tooltip-id="getBack"
+                            data-tooltip-content={'Retour'}/>
+                        Match {matchInfo.teamId1.name} <span className="mr-2 ml-2 text-accent"> vs </span> {matchInfo.teamId2.name}
+                    </h2>
+                    <Tooltip id="getBack" />
+                    <div className="flex flex-col mt-10">
+                        <div className="grid grid-flow-col justify-stretch mt-10 h-fit w-full place-items-center">
+                            <div className="flex flex-col space-y-8">
+                                <img src={logoTeam1} alt="" className="h-72 drop-shadow-logo" />
+                                <button
+                                    className="bg-accent text-white font-bold py-2 px-4 rounded"
+                                    onClick={() => openPopup(matchInfo.teamId1.name)}
+                                    data-tooltip-id="betTeam1"
+                                    data-tooltip-content={`Parier sur ${matchInfo.teamId1.name}`}
+                                >
+                                    <Tooltip id="betTeam1" />
+                                    {matchInfo.teamId1.name}
+                                </button>
+                            </div>
+                            <p className="text-accent text-6xl">VS</p>
+                            <div className="flex flex-col space-y-4">
+                                <img src={logoTeam2} alt="" className="h-72 drop-shadow-logo" />
+                                <button
+                                    className="bg-accent text-white font-bold py-2 px-4 rounded"
+                                    onClick={() => openPopup(matchInfo.teamId2.name)}
+                                    data-tooltip-id="betTeam2"
+                                    data-tooltip-content={`Parier sur ${matchInfo.teamId2.name}`}
+                                >
+                                    <Tooltip id="betTeam2" />
+                                    {matchInfo.teamId2.name}
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                    <p className="text-accent text-6xl">VS</p>
-                    <div className="flex flex-col space-y-4">
-                        <img src={logoTeam2} alt="" className="h-72 drop-shadow-logo" />
-                        <button
-                            className="bg-accent text-white font-bold py-2 px-4 rounded"
-                            onClick={() => openPopup(matchInfo.teamId2)}
-                            data-tooltip-id="betTeam2"
-                            data-tooltip-content={`Parier sur ${matchInfo.teamId2}`}
-                        >
-                            <Tooltip id="betTeam2" />
-                            {matchInfo.teamId2}
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            {isPopupOpen && (
-                <Popup onClose={closePopup} 
-                team={teamToBet}/>
+                    {isPopupOpen && (<Popup onClose={closePopup} team={teamToBet}/>)}
+                </div> 
             )}
         </div>
     )
